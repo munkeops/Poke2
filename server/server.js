9ec1d633107e3035fabc694a9e7aa004548d0f95
AbdulKhaliq <thedjawking@gmail.com>
2020-05-29 15:36:37 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ let playerId = 0;
 function damagecalc(attackObject,defendingPoke){
 
   let damage_1 = (((2*10)/5)+2) * attackObject.move.pow ;            //the calculation was split up in parts for better readability although it is the same as on the referenced website.
-  let attackingPoke = pokemon1.find(pokemon=>pokemon1.active===1);
+  let attackingPoke = attackingPoke.find(pokemon=>pokemon.active===1);
   let damage_2 = (attackingPoke.stats.atk/defendingPoke.stats.def);
   damage = (((damage_1*damage_2)/50)+2) * attackObject.stats.mod;     // all calculations here are done in favor of "me" team due to it having higher speed
   return damage;
@@ -314,6 +314,8 @@ io.on("connection", (socket) => {
               enemyPoke: enemyTeam.team[enemyIndex],
               username,
             });
+
+            /*
           } else if (myStats.spe < enemyStats.spe){
             myStats.hp -= damagecalc(enemyTeam,selectedPoke); //TODO Inverse of previous case. calculate hp reduction for "myStats" based on "enemyStats" first
 
@@ -362,6 +364,7 @@ io.on("connection", (socket) => {
               username,
             });
           }
+          
           else if (myStats.spe === enemyStats.spe){
             
           rand = Math.floor(Math.random() * 2)
@@ -422,6 +425,7 @@ io.on("connection", (socket) => {
             });
 
           }
+          */
 
         }
       }
